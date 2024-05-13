63d8a09ec5e3364efb3808a01dd7090a3230f13b
Alva <xshinoa0208@gmail.com>
2024-05-14 01:46:39 +0800

    otherMicroRevise

--- js/otherJs.js
+++ js/otherJs.js
@@ -3,6 +3,7 @@ let menu_windowcontent=document.querySelector("#menu_windowcontent");
 let btn_menumb=document.querySelector("#btn_menumb");
 let isMenuClick=false;
 
+
 $(document).ready(function () {
 
     $('#menutoprect').on('click',function () {
@@ -23,4 +24,38 @@ $(document).ready(function () {
 
 function navigateToMainPage(){
     window.open("index.html#main", "_self"); 
-  }
+};
+
+
+let cloud1MovingTimeline = gsap.timeline({repeat:-1});
+let cloud2MovingTimeline = gsap.timeline({repeat:-1});
+let cloud3MovingTimeline = gsap.timeline({repeat:-1});
+let cloud4MovingTimeline = gsap.timeline({repeat:-1});
+
+function cloud1Moving(){
+    cloud1MovingTimeline
+        .fromTo("#cloud1",2,{x:0},{x:40})
+        .fromTo("#cloud1",2,{x:40},{x:0}); 
+}
+function cloud2Moving(){
+    cloud2MovingTimeline
+        .fromTo("#cloud2",1.5,{x:0},{x:-40})
+        .fromTo("#cloud2",1.5,{x:-40},{x:0}); 
+}
+function cloud3Moving(){
+    cloud3MovingTimeline
+        .fromTo("#cloud3",2,{y:0},{y:20})
+        .fromTo("#cloud3",2,{y:20},{y:0}); 
+}
+function cloud4Moving(){
+    cloud4MovingTimeline
+        .fromTo("#cloud4",1.5,{y:0},{y:-20})
+        .fromTo("#cloud4",1.5,{y:-20},{y:0}); 
+}
+
+window.onload = function(){
+    cloud1Moving();
+    cloud2Moving();
+    cloud3Moving();
+    cloud4Moving();
+}
